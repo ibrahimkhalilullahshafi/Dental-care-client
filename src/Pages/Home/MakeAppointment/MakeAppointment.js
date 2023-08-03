99ecf0d415a07532d305e596c71106d317d9315e
ibrahimkhalilullahshafi <shafi.warx@gmail.com>
2023-08-03 19:28:41 +0600

    appointment section added

--- src/Pages/Home/MakeAppointment/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import doctor from '../../../assets/images/doctorx.png'
-
+import doctor from '../../../assets/images/doctor 1.png'
+import background from '../../../assets/images/appointment.png'
 const MakeAppointment = () => {
     return (
-        <div className="card lg:card-side bg-base-100 shadow-xl">
+        <div className="card lg:card-side bg-base-100 shadow-xl"
+            style={{
+                backgroundImage: `url(${background})`,
+            }}
+        >
             <figure><img src={doctor} alt="" /></figure>
             <div className="card-body">
-                <h2 className="card-title">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Listen</button>
-                </div>
+                <h2 className="card-title text-white">Make an appointment today</h2>
+                <p className='text-white'>Helping patients achieve good dental health & beautiful smile is a privilege & responsibility. For over 10 years, my team & I have proudly provided the best dental experience in Bangladesh. Our comfort-first approach is designed to meet the needs of you & your entire family.</p>
+                <button className="btn btn-primary">Appointment</button>
             </div>
+
         </div>
     );
 };
