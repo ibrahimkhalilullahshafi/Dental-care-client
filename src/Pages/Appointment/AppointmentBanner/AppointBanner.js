b7a4c7145ddb41207921981280973820c621d69e
ibrahimkhalilullahshafi <shafi.warx@gmail.com>
2023-08-09 20:54:36 +0600

    some editing in appoinment banner

--- src/Pages/Appointment/AppointmentBanner/AppointBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointBanner.js
@@ -7,13 +7,12 @@ import { format } from 'date-fns';
 const AppointBanner = () => {
     const [selectedDate, setSelectedDate] = useState(new Date());
     return (
-        <div className="hero min-h-screen bg-base-100" style={{
+        <div className="hero bg-base-100 place-items-stretch w-screen" style={{
             backgroundImage: `url(${background})`,
         }}>
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='' />
-                <div>
-                    <h1 className="text-5xl font-bold">appoint now</h1>
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl mx-auto" alt='' />
+                <div className='mx-auto'>
                     <DayPicker
                         mode='single'
                         selected={selectedDate}
